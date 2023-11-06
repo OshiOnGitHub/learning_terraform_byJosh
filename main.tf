d6fa72eae020c4ac2c45ea2ae165a2cf290a5581
Oshi <146834773+OshiOnGitHub@users.noreply.github.com>
2023-11-06 19:12:12 +0530

    Load Balancer

--- main.tf
+++ main.tf
@@ -41,9 +41,48 @@ resource "aws_instance" "blog" {
 
   subnet_id = module.blog_vpc.public_subnets[0]
 
-
   tags = {
     Name = "Learning Terraform"
+  }
+}
+
+module "alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 6.0"
+
+  name = "blog-alb"
+
+  load_balancer_type = "application"
+
+  vpc_id          = module.blog_vpc.vpc_id
+  subnets         = module.blog_vpc.public_subnets
+  security_groups = module.blog_sg.security_group_id
+
+  target_groups = [
+    {
+      name_prefix = "blog-"
+      backend_protocal = "HTTP"
+      backend_port = 80
+      target_type = "instance"
+      target = {
+        my_target = {
+          target_id = aws_instance.blog.id
+          port = 80
+        }
+      }
+    }
+  ]
+
+  https_listeners = [
+    {
+      port = 80
+      protocol = "HTTP"
+      target_group_index = 0
+    }
+  ]
+
+  tags ={
+    Environment = "dev"
   }
 }
 
